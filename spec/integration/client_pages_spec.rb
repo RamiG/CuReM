6456355c907464705bb2bf3dd5c85979ddf19042
Ramil Gilmanov <ramil1985@mail.ru>
2014-10-27 17:12:14 +0400

    Improve client specs

--- spec/integration/client_pages_spec.rb
+++ spec/integration/client_pages_spec.rb
@@ -5,17 +5,51 @@ describe 'Client pages' do
   let!(:client2) { FactoryGirl.create(:client) }
   let!(:client3) { FactoryGirl.create(:client) }
 
-  before do
-    sign_in(admin)
-    visit admin_clients_path
-  end
+  before { sign_in(admin) }
 
   subject { page }
 
-  describe 'index' do
+  context 'index' do
+    before { visit admin_clients_path }
+
     it { should have_css('#page_title', text: 'Clients') }
     it { should have_link(client1.id, href: admin_client_path(client1)) }
     it { should have_link(client2.id, href: admin_client_path(client2)) }
     it { should have_link(client3.id, href: admin_client_path(client3)) }
+
+    context 'view lient' do
+      before { click_link(client2.id, href: admin_client_path(client2)) }
+
+      it { should have_css('#page_title', text: client2.full_name) }
+      it { should have_css('tr', text: client2.first_name) }
+      it { should have_css('tr', text: client2.last_name) }
+      it { should have_css('tr', text: client2.birthdate.strftime('%B %d, %Y')) }
+      it { should have_css('tr', text: client2.email) }
+      it { should have_css('tr', text: client2.phone) }
+      it { should have_css('tr', text: client2.terms_accepted) }
+    end
+  end
+
+  context 'new client' do
+    let(:new_client) { FactoryGirl.build(:client) }
+
+    before do
+      visit new_admin_client_path
+
+      fill_in 'First name', with: new_client.first_name
+      fill_in 'Last name', with: new_client.last_name
+      select '1950', from: 'client_birthdate_1i'
+      select 'March', from: 'client_birthdate_2i'
+      select '19', from: 'client_birthdate_3i'
+      fill_in 'Email', with: new_client.email
+      fill_in 'Phone', with: new_client.phone
+      check 'Terms accepted'
+      click_button 'Create Client'
+    end
+
+    it 'should create new client' do
+      expect(page).to have_css('div.flash_notice')
+      expect(Client.count).to eq(4)
+    end
   end
 end
